12f6a8afdda8630fa6bea8143897d4ab9e8eeebc
Renan Dias <renandias26@outlook.com.br>
2024-08-28 21:05:02 -0300

    Causando erro proposital

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,4 +6,4 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+    assert response.json() == {"Hello": "teste"}
